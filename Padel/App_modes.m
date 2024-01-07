f17cc6e707877f7f48f7cee9d606e133e67de969
alaa2saied <146351933+alaa2saied@users.noreply.github.com>
2024-01-07 18:04:24 +0200

    Delete Padel directory

--- Padel/App_modes.m
+++
@@ -1,8 +0,0 @@
-classdef (Enumeration) App_modes < Simulink.IntEnumType
-    enumeration
-        Normal(0)
-        Downgraded_sen_1(1)
-        Downgraded_sen_2(2)
-        Failure(3)
-    end
-end

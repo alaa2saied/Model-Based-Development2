0c2690d494d9a769e0227fcff72f4071854955d0
alaa2saied <146351933+alaa2saied@users.noreply.github.com>
2024-01-07 17:52:46 +0200

    Add files via upload

---
+++ Simulink_basics/untitled.c
@@ -0,0 +1,49 @@
+/*
+ * File: untitled.c
+ *
+ * Code generated for Simulink model 'untitled'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+ * C/C++ source code generated on : Fri Oct 27 12:02:03 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include "untitled.h"
+
+/* External inputs (root inport signals with default storage) */
+ExtU rtU;
+
+/* External outputs (root outports fed by signals with default storage) */
+ExtY rtY;
+
+/* Real-time model */
+static RT_MODEL rtM_;
+RT_MODEL *const rtM = &rtM_;
+
+/* Model step function */
+void untitled_step(void)
+{
+  /* Outport: '<Root>/Outport' incorporates:
+   *  Inport: '<Root>/Inport'
+   */
+  rtY.Outport = rtU.Inport;
+}
+
+/* Model initialize function */
+void untitled_initialize(void)
+{
+  /* (no initialization code required) */
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ Simulink_basics/untitled.h
@@ -0,0 +1,90 @@
+/*
+ * File: untitled.h
+ *
+ * Code generated for Simulink model 'untitled'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+ * C/C++ source code generated on : Fri Oct 27 12:02:03 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_untitled_h_
+#define RTW_HEADER_untitled_h_
+#ifndef untitled_COMMON_INCLUDES_
+#define untitled_COMMON_INCLUDES_
+#include "rtwtypes.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+#endif                                 /* untitled_COMMON_INCLUDES_ */
+
+/* Macros for accessing real-time model data structure */
+#ifndef rtmGetErrorStatus
+#define rtmGetErrorStatus(rtm)         ((rtm)->errorStatus)
+#endif
+
+#ifndef rtmSetErrorStatus
+#define rtmSetErrorStatus(rtm, val)    ((rtm)->errorStatus = (val))
+#endif
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM RT_MODEL;
+
+/* External inputs (root inport signals with default storage) */
+typedef struct {
+  real_T Inport;                       /* '<Root>/Inport' */
+} ExtU;
+
+/* External outputs (root outports fed by signals with default storage) */
+typedef struct {
+  real_T Outport;                      /* '<Root>/Outport' */
+} ExtY;
+
+/* Real-time Model Data Structure */
+struct tag_RTM {
+  const char_T * volatile errorStatus;
+};
+
+/* External inputs (root inport signals with default storage) */
+extern ExtU rtU;
+
+/* External outputs (root outports fed by signals with default storage) */
+extern ExtY rtY;
+
+/* Model entry point functions */
+extern void untitled_initialize(void);
+extern void untitled_step(void);
+
+/* Real-time Model object */
+extern RT_MODEL *const rtM;
+
+/*-
+ * The generated code includes comments that allow you to trace directly
+ * back to the appropriate location in the model.  The basic format
+ * is <system>/block_name, where system is the system number (uniquely
+ * assigned by Simulink) and block_name is the name of the block.
+ *
+ * Use the MATLAB hilite_system command to trace the generated code back
+ * to the model.  For example,
+ *
+ * hilite_system('<S3>')    - opens system 3
+ * hilite_system('<S3>/Kp') - opens and selects block Kp which resides in S3
+ *
+ * Here is the system hierarchy for this model
+ *
+ * '<Root>' : 'untitled'
+ * '<S1>'   : 'untitled/Subsystem'
+ */
+#endif                                 /* RTW_HEADER_untitled_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
